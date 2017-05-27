6b5afacb1069d6a367db46366097bd1772271952
All-P <pol.padilla74@vilanova.escolateresiana.com>
2017-05-28 00:59:04 +0200

    Custom plugins

---
+++ Videojoc PR/js/plugins/POL_CustomMenu.js
@@ -0,0 +1,72 @@
+/*:
+*
+* @plugindesc Custom Menu
+* 
+* @author All-P
+*
+* @help
+*/
+
+(function(){
+
+	var _Scene_Menu_create = Scene_Menu.prototype.create;
+	Scene_Menu.prototype.create = function() {
+		Scene_MenuBase.prototype.create.call(this);
+		_Scene_Menu_create.call(this);
+		/*
+		this._statusWindow
+		this._commandWindow
+		this._goldWindow
+		*/
+
+		this._commandWindow.x = 0; //this._statusWindow.width;
+		this._commandWindow.y = 0;
+
+		this._statusWindow.x = 0;
+		this._statusWindow.y = this._commandWindow.height;
+
+		this._goldWindow.x = this._statusWindow.width;
+		this._goldWindow.y = this._commandWindow.height;
+		this._goldWindow.opacity = 0;
+		};
+
+	// ----------------------
+	// MENU STATUS
+	//-----------------------
+	Window_MenuStatus.prototype.windowWidth = function() {
+	    return Graphics.boxWidth - 240;
+	};
+
+	Window_MenuStatus.prototype.windowHeight = function() {
+	    return Graphics.boxHeight / 2;
+	};
+
+	Window_MenuStatus.prototype.numVisibleRows = function() {
+	    return 2;
+	};
+
+	Window_MenuStatus.prototype.maxCols = function() {
+	    return 1;
+	};
+
+	// ----------------------
+	// MENU COMMAND
+	//-----------------------
+
+	Window_MenuCommand.prototype.windowWidth = function() {
+    return Graphics.boxWidth;
+	};
+
+	Window_MenuCommand.prototype.windowHeight = function() {
+    return this.fittingHeight(this.numVisibleRows());
+	};
+
+	Window_MenuCommand.prototype.numVisibleRows = function() {
+	    return this.maxItems() / 4;
+	};
+
+	Window_MenuCommand.prototype.maxCols = function() {
+	    return 4;
+	};
+
+})();
